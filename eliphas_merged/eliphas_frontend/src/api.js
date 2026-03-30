eb0154151e4e61cf4a3fc3f8387dd75fb23c0dba
Manikanta222003 <manikantakedarisetti3@gmail.com>
2026-03-30 08:50:26 +0530

    Update api.js

--- eliphas_merged/eliphas_frontend/src/api.js
+++ eliphas_merged/eliphas_frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://eliphas-com.onrender.com/api'  // 👈 replace with your Render URL
 })
 
 API.interceptors.request.use((req) => {
